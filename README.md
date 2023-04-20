bc065a887d014bcba3032033bcf7f593e256102e
dodo920306 <dodo920306@gmail.com>
2023-04-20 19:16:33 +0800

    Initialized the project
    Create main smart contract based on ERC-1155 standard from OpenZeppelin-contracts-upgradeable for its security and completeness.
    Add modifier onlyPublisherBatch to restrict batch minting access to publishers of corresponding book ids.
    Implement functions grantPublisher and setRoleAdminAsPublisherItself to enable basic publisher role access control.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# OharaProtocol
- 
+# The_Ohara_Protocol
+## Overview
+ This repository is the source code of the Ohara Protocol, which indicates to be an E-Book trading system based on ERC-1155 standard on the Arbitrum blockchain. It allows publishers to mint their E-Books as NFTs and sell them to buyers. The system utilizes the Arbitrum chain for its high throughput and low transaction fees, providing a seamless and efficient E-Book trading experience for all parties involved.
+ This project is still under development, and we welcome any suggestions for improvement. You can help us identify issues by using the "Issues" tab above. Your feedback is valuable and we appreciate your contribution to the project's development.
+## Installation
+ Please use clone this repository and run
+ ```bash
+ npm install
+ ```
+ under the project root directory.
+ Now, you can use truffle to develop and test this project.
---
+++ contracts/The_Ohara_Protocol.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/// @custom:security-contact dodo920306@gmail.com
+contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable {
+    mapping (uint256 => bytes32) private _idToPublisher;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    // There's no onlyPublisherSingle, for that it basically equals to onlyRole modifier from AccessControl. 
+    modifier onlyPublisherBatch(uint256[] memory ids) {
+        for (uint256 i = 0; i < ids.length; ++i) {
+            _checkRole(_idToPublisher[ids[i]]);
+        }
+        _;
+    }
+
+    function initialize() initializer public {
+        __ERC1155_init("");
+        __AccessControl_init();
+        __Pausable_init();
+        __ERC1155Burnable_init();
+        __ERC1155Supply_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setURI(string memory newuri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setURI(newuri);
+    }
+
+    /**
+     * @dev The publisher who wants to claims an account belongs to them must use this function
+     * with the magic string that represents themselves.
+     * The magic string should conform to the following format:
+     * it must be in English,
+     * all in uppercase,
+     * and spaces should be replaced by underscores.
+     * The content of the string can be arbitrary, but for convenience, it is recommended to use a meaningful publisher name.
+     * For instance, the magic string for 誠品書店 would be ESLITE_BOOKSTORE.
+     * 
+     * The function will uses keccak256 solidity function to hash this string, but before that, it must be represented as bytes32.
+     * The type conversionfrom "string memory" to "bytes32" isn't allowed, so we uses abi.encode() here.
+     * See https://ethereum.stackexchange.com/questions/119583/when-to-use-abi-encode-abi-encodepacked-or-abi-encodewithsignature-in-solidity
+     * for the reason of this selection.
+     * After that, we use grantRole() function from AccessControl,
+     * the reason we don't directly use _grantRole() here is because the onlyRole(getRoleAdmin(role)) should also be passed in this operation.
+     */
+    function grantPublisher(string memory publisher, address account) public virtual {
+        bytes32 role = keccak256(abi.encode(publisher));
+        grantRole(role, account);
+    }
+
+    /**
+     * @dev See the comment of grantPublisher() above for more info about the string publisher and the bytes32 role.
+     * This function should be invoked after a publisher has registered (the default roleAdmin for any role is DEFAULT_ADMIN_ROLE),
+     * indicating that from this point on, only the publisher can manage which accounts can act on its behalf.
+     */
+    function setRoleAdminAsPublisherItself(string memory publisher) public virtual {
+        bytes32 role = keccak256(abi.encode(publisher));
+        _setRoleAdmin(role, role);
+    }
+
+    function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyRole(_idToPublisher[id])
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyPublisherBatch(ids)
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155Upgradeable, ERC1155SupplyUpgradeable)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
