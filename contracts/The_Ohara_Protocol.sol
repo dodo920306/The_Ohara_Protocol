fea390b14ae14f0348892f24c1286951e4730abb
dodo920306 <dodo920306@gmail.com>
2023-04-21 22:34:18 +0800

    Change the way to manage admin role
    Delete setPublisherAdminAsItself, for that
    the procedure that set the publisher's admin to itself should be call
    only after the first member of it has been added, or there would be a
    publisher that will never have a member.
    Hence, the procedure should be written into the first time granting the
    publisher.
    Change the grantPublisher function to satisfy that.
    Change the function's comment to detail the power of DEFAULT_ADMIN_ROLE
    and why the publishers already be registered won't be affected by it.

--- contracts/The_Ohara_Protocol.sol
+++ contracts/The_Ohara_Protocol.sol
@@ -10,7 +10,8 @@ import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initiali
 
 /// @custom:security-contact dodo920306@gmail.com
 contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable {
-    mapping (uint256 => bytes32) private _idToPublisher;
+    mapping (uint256 => bytes32) idToPublisher; // Anyone can check which publisher an id belongs to.
+    mapping (string => bool) publisherHasMember;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -20,7 +21,7 @@ contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlU
     // There's no onlyPublisherSingle, for that it basically equals to onlyRole modifier from AccessControl. 
     modifier onlyPublisherBatch(uint256[] memory ids) {
         for (uint256 i = 0; i < ids.length; ++i) {
-            _checkRole(_idToPublisher[ids[i]]);
+            _checkRole(idToPublisher[ids[i]]);
         }
         _;
     }
@@ -42,12 +43,6 @@ contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlU
     /**
      * @dev The publisher who wants to claim an account belongs to them must use this function
      * with the magic string that represents themselves.
-     * The magic string should conform to the following format:
-     * it must be in English,
-     * all in uppercase,
-     * and spaces should be replaced by underscores.
-     * The content of the string can be arbitrary, but for convenience, it is recommended to use a meaningful publisher name.
-     * For instance, the magic string for 誠品書店 would be ESLITE_BOOKSTORE.
      * 
      * The function will uses keccak256 solidity function to hash this string, but before that, it must be represented as bytes32.
      * The type conversion from "string memory" to "bytes32" isn't allowed, so we uses abi.encode() here.
@@ -56,26 +51,29 @@ contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlU
      * 
      * After that, we use grantRole() function from AccessControl,
      * the reason we don't directly use _grantRole() here is because the onlyRole(getRoleAdmin(role)) should also be passed in this operation,
-     * and since grantRole() has that modifier originally, the current function doesn't require any modifier yet.
+     * and since grantRole() has that modifier originally, the current function doesn't require any modifier yet,
+     * and there's no need to be afraid that anyone hash others' magic string and directly call grantRole() because there's no way he/she can be the admin.
+     * 
+     * If it's the first member to be added in the publisher,
+     * the msg.sender must be DEFAULT_ADMIN_ROLE to pass onlyRole(getRoleAdmin(role)) for that there's no way to set admin to others.
+     * If so, then the function would call _setRoleAdmin() to release the admin power to the publisher itself,
+     * indicating that from this point on, only the publisher can manage which accounts can act on its behalf.
+     * 
+     * The only risk would be that a hacker steal one private key from DEFAULT_ADMIN_ROLE,
+     * in that case, the worse he/she can do is grant his/her accounts the publisher role that "hasn't been registered yet",
+     * for that the admins of the publishers already in the protocol can only be themselves.
+     * If so, then the publishers register in the future cannot use the same magic string as the hacker granted himself/herself even if they want.
+     * (Basically, it's the same idea as "This username has already been used. Please try another.",
+     * so the power of DEFAULT_ADMIN_ROLE is basically just register publishers and ids.)
      */
     function grantPublisher(string memory publisher, address account) public virtual {
         bytes32 role = keccak256(abi.encode(publisher));
         grantRole(role, account);
-    }
 
-    /**
-     * @dev See the comment of grantPublisher() above for more info about the string publisher and the bytes32 role.
-     * This function should be invoked after a publisher has registered (the default admin role for any role is DEFAULT_ADMIN_ROLE),
-     * indicating that from this point on, only the publisher can manage which accounts can act on its behalf.
-     * 
-     * The current function doesn't require any modifier yet,
-     * for that it simply sets the admin role for the role represented by the magic string to itself.
-     * Even if someone naughty enters others' magic string to this function,
-     * the worst he can do is setting the victims' admin role to the victims themselves, which isn't a significant concern.
-     */
-    function setRoleAdminAsPublisherItself(string memory publisher) public virtual {
-        bytes32 role = keccak256(abi.encode(publisher));
-        _setRoleAdmin(role, role);
+        if (!publisherHasMember[publisher]){
+            publisherHasMember[publisher] = true;
+            _setRoleAdmin(role, role);
+        }
     }
 
     /**
@@ -85,10 +83,21 @@ contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlU
      */
     function setIdToPublisher(uint256 id, string memory publisher)
         public
-        onlyRole(_idToPublisher[id])
+        onlyRole(idToPublisher[id])
     {
         bytes32 role = keccak256(abi.encode(publisher));
-        _idToPublisher[id] = role;
+        idToPublisher[id] = role;
+    }
+
+    /**
+     * @dev See the comment of grantPublisher() above for more info about the string publisher and the bytes32 role.
+     * The reason we don't directly call _revokeRole() and the this function is safe is the same reason grantPublisher() doesn't call _grantRole().
+     * Use this function to kick someone including msg.sender himself/herself out of the publisher.
+     * The function also can only be called by the publisher itself.
+     */
+    function revokePublisher(string memory publisher, address account) public virtual {
+        bytes32 role = keccak256(abi.encode(publisher));
+        revokeRole(role, account);
     }
 
 
@@ -103,7 +112,7 @@ contract The_Ohara_Protocol is Initializable, ERC1155Upgradeable, AccessControlU
     // The publisher of the id would have power to mint the id to whoever they want.
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
         public
-        onlyRole(_idToPublisher[id])
+        onlyRole(idToPublisher[id])
     {
         _mint(account, id, amount, data);
     }
