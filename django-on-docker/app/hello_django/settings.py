e22a0b14aca4d4e20e666698c07ec704f59e5b6e
dodo920306 <dodo920306@gmail.com>
2023-04-23 01:08:52 +0800

    Fix settings.py
    There are some mistakes there.

--- django-on-docker/app/hello_django/settings.py
+++ django-on-docker/app/hello_django/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-pcruv244_9y95cwi$vs7-+4eoi(9=2^jag-)rrj+8+4=r!j#j%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get("SECRET_KEY")
